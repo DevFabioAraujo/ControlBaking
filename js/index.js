2da90d17ba4165b5ed353578fb3b7c43f02ac76b
DevFabioAraujo <devsystempro@outlook.com>
2024-09-04 20:54:03 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+       
+        document.getElementById("btn1").onclick = function() {
+            window.location.href = 'bakingSmall.html';
+        }; 
+    
+        document.getElementById("btn2").onclick = function() {
+            window.location.href = 'bakingBig.html';
+        };
+ 
